5d99b3fc1244dabc56a516b76219afa34112d6ec
adit <adytiakristanto893.com>
2020-11-04 13:26:54 +0700

    Master connection v4.5/fix swal

--- resources/views/master/koneksi/index.blade.php
+++ resources/views/master/koneksi/index.blade.php
@@ -1,141 +1,168 @@
 @extends('layout.main')
 @section('title')
-  <span>Master Koneksi</span>
-  <a href="javascript:void()" onclick="create()" class="btn btn-success btn-sm rounded-circle"><i class="fa fa-plus-circle"></i></a>
+	<span>Master Koneksi</span>
+	<a href="javascript:void()" onclick="create()" class="btn btn-success btn-sm rounded-circle"><i class="fa fa-plus-circle"></i></a>
 @endsection
 @section('content')
-  <div id="connections"></div>            
+	<div id="connections"></div>            
 @endsection
 @section('js')
-  <script>
-      function create(){
-        $.ajax({
+	<script>
+    	function create(){
+    	$.ajax({
             url: '<?= route('koneksi.create') ?>',
             success: function(response){
-              bootbox.dialog({
-                title: 'create',
-                message: response
-              });
+            	bootbox.dialog({
+                	title: 'create',
+                	message: response
+              	});
             }
         });
-      }
+      	}
 
-      $(function(){
-        get();
-      });
+    	$(function(){
+        	get();
+      	});
 
-      function get(){
-        $.ajax({
-          url: '<?= route('koneksi.get') ?>',
-          success: function(response){
-            $('#connections').html(response);
-          }
+    	function get(){
+        	$.ajax({
+          	url: '<?= route('koneksi.get') ?>',
+          	success: function(response){
+            	$('#connections').html(response);
+          	}
         });
-      }
+      	}
 
-      function edit(id){
-        $.ajax({
-          url: '<?= route('koneksi.edit') ?>/'+id,
-          success: function(response){
-            bootbox.dialog({
-                title: 'edit',
-                message: response
-              });
-          }
-        });
-      }
+    	function edit(id){
+        	$.ajax({
+        	url: '<?= route('koneksi.edit') ?>/'+id,
+        	success: function(response){
+            	bootbox.dialog({
+                	title: 'edit',
+                	message: response
+              	});
+          	}
+        	});
+      	}
 
-      function update(id){
-        $('#form_koneksi .alert').remove();
-        $.ajax({
-          url: '<?= route('koneksi.update') ?>/'+id,
-          dataType: 'json',
-          type: 'post',
-          data: $('#form_koneksi').serialize(),
-          success: function(response){
-            if(response.success){
-                    swal({
-                    title: "Create",
+    	function update(id){
+        	$('#form_koneksi .alert').remove();
+        	$.ajax({
+          	url: '<?= route('koneksi.update') ?>/'+id,
+          	dataType: 'json',
+          	type: 'post',
+          	data: $('#form_koneksi').serialize(),
+          	success: function(response){
+            	if(response.success){
+                    Swal.fire({
+                    title: 'Store',
                     text: response.message,
-                    icon: "success",
-                    button: "Oke"
+                    icon: 'success',
+                    confirmButtonColor: '#2c91fb',
+                    confirmButtonText: 'Oke'
                     });
-                    bootbox.hideAll();
-                    get();
-            }else{
-              swal({
-                    title: "Create",
+                }else{
+                    Swal.fire({
+                    title: 'Error',
                     text: response.message,
-                    icon: "warning",
-                    button: "Oke"
+                    icon: 'error',
+                    confirmButtonColor: '#d33',
+                    confirmButtonText: 'Oke'
                     });
-            }
-          },
-          error: function(xhr, ajaxOptions, thrownError){
-            var response = JSON.parse(xhr.responseText);
-            $('#form_koneksi').prepend(validation(response));
-          }
-        });
-      }
+                }
+            	bootbox.hideAll();
+        		get();
+            },
+            error: function(xhr, ajaxOptions, thrownError){
+            	var response = JSON.parse(xhr.responseText);
+            	$('#form_koneksi').prepend(validation(response));
+        	}
+        	});
+    	}
 
-      function store(){
-        $('#form_koneksi .alert').remove();
-        $.ajax({
-          url: '<?= route('koneksi.store'); ?>',
-          dataType: 'json',
-          type: 'post',
-          data: $('#form_koneksi').serialize(),
-          success: function(response){
-            if(response.success){
-              alert(response.message);
-              bootbox.hideAll();
-              get();
-            }else{
-              alert(response.message);
-            }
-          },
-          error: function(xhr, ajaxOptions, thrownError){
-            var response = JSON.parse(xhr.responseText);
-            $('#form_koneksi').prepend(validation(response));
-          }
-        });
-      }
-
-      function destroy(id){
-        $.ajax({
-          url: '<?= route('koneksi.delete') ?>/'+id,
-          dataType: 'json',
-          success: function(response){
-            if(response.success){
-                swal({
-                    title: "delete",
+    	function store(){
+        	$('#form_koneksi .alert').remove();
+        	$.ajax({
+          	url: '<?= route('koneksi.store'); ?>',
+          	dataType: 'json',
+          	type: 'post',
+          	data: $('#form_koneksi').serialize(),
+          	success: function(response){
+            	if(response.success){
+                    Swal.fire({
+                    title: 'Store',
                     text: response.message,
-                    icon: "success",
-                    button: "Oke"
+                    icon: 'success',
+                    confirmButtonColor: '#2c91fb',
+                    confirmButtonText: 'Oke'
                     });
-                    get();
-                    bootbox.hideAll();
-            }else{
-              swal({
-                    title: "delete",
+                }else{
+                    Swal.fire({
+                    title: 'Error',
                     text: response.message,
-                    icon: "warning",
-                    button: "Oke"
+                    icon: 'error',
+                    confirmButtonColor: '#d33',
+                    confirmButtonText: 'Oke'
                     });
-                    bootbox.hideAll();
-            }
-          }
-        });
-      }
+                }
+                bootbox.hideAll();
+                get();
+            },
+            error: function(xhr, ajaxOptions, thrownError){
+            	var response = JSON.parse(xhr.responseText);
+            	$('#form_koneksi').prepend(validation(response));
+        	}
+        	});
+    	}
 
-      function validation(errors){
-        var validation = '<div class="alert alert-danger">';
-            validation += '<p><b>'+errors.message+'</b></p>';
-            $.each(errors.errors, function(i, error){
-              validation += error[0]+'<br>';
-            });
-            validation += '</div>';
-            return validation;
-      }
-  </script>
+    	function destroy(id){
+    		Swal.fire({
+          	title: 'Delete',
+          	text: 'Apakah anda yakin akan menghapus data ini?',
+          	icon: 'warning',
+          	showCancelButton: true,
+          	confirmButtonColor: '#d33',
+          	cancelButtonColor: '#929ba1',
+          	confirmButtonText: 'Oke'
+          	}).then((result) => {
+            	if (result.value) {         
+                	$.ajax({
+                  	url: '<?= route('koneksi.delete') ?>/'+id,
+                  	success: function(response) {
+                  	dataTable.ajax.reload();
+                    	Swal.fire({
+                      	title : 'Terhapus!',
+                      	icon: 'success',
+                      	text: response.message,
+                    	})
+                  	},
+                  	error: function(){
+                    	Swal.fire({
+                      	title: 'Error!',
+                      	text: response.message,
+                      	type: 'error'
+                    	})
+                  	}        
+                	});
+              	get();
+            	}else if (result.dismiss === Swal.DismissReason.cancel) {
+                	Swal.fire(
+                	'Cancelled',
+                	'Data tidak jadi dihapus',
+                	'error'
+              		)
+            	}
+        	});
+      	}
+
+    	function validation(errors){
+        	var validation = '<div class="alert alert-danger">';
+            	validation += '<p><b>'+errors.message+'</b></p>';
+            	$.each(errors.errors, function(i, error){
+              		validation += error[0]+'<br>';
+            	});
+            	validation += '</div>';
+            	return validation;
+      	}
+	</script>
 @endsection
