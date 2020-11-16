b6b8df474563a3a5b379c44b68226aef90a081ea
kresna <kresnacahyono7@gmail.com>
2020-11-16 17:49:05 +0700

    WF-128: Master Objek

--- resources/views/start.blade.php
+++ resources/views/start.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="{!! asset('template') !!}/img/logo.png">
     <link href="{!! asset('template') !!}/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="{!! asset('template') !!}/css/sb-admin-2.min.css" rel="stylesheet">
     <title>Document Generator</title>
