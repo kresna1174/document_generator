fbfed27d7b97276d7b5f172dbd810cc4a0bace32
kresna <kresnacahyono7@gmail.com>
2020-11-16 11:29:43 +0700

    WF-128 & WF-129: Migration

---
+++ database/migrations/2020_11_16_042440_jenis_dokumen.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class JenisDokumen extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jenis_dokumen', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nama_surat', 64);
+            $table->string('file', 255);
+            $table->integer('id_objek', 11);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jenis_dokumen');
+    }
+}
