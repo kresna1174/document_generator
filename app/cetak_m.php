a1fea07201d5b67af4f30bcee09fa560cce15b61
kresna <kresnacahyono7@gmail.com>
2020-11-25 16:09:28 +0700

    WF-130 : error fixed

--- app/cetak_m.php
+++ app/cetak_m.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class cetak_m extends objek_m
+class cetak_m extends Model
 {
     protected $table = 'cetak';
     protected $guarded = [];
