251667dda0d8fb18ee8ad2aa89af6389feebc9d4
Kresna <kresnacahyono7@gmail.com>
2020-12-03 13:01:54 +0700

    WF-145: Parameter Menu Master

--- resources/views/master/jenis_dokumen/form.blade.php
+++ resources/views/master/jenis_dokumen/form.blade.php
@@ -10,5 +10,30 @@
     <label>Upload</label>
     {!! Form::file('file_world', ['class' => 'form-control-file card shadow pt-2 pl-2 pb-2' ,'id' => 'file_world']) !!}
 </div>
+<table id="table-list" class="table table-bordered table-consoned">
+    <thead>
+        <tr>
+            <th>Nama Table</th>
+            <th>Nama Kolom</th>
+        </tr>
+    </thead>
+    <tbody>
+
+    </tbody>
+</table>
+<script>
+    $(function(){
+        $('#table-list').DataTable({
+            processing: true,
+            searchDelay: 1000,
+            serverSide: true,
+            ajax: '<?= route('objek.get_data') ?>',
+            columns:[
+                {data: 'nama_table', name: 'nama_table'},
+                {data: 'nama_kolom', name: 'nama_kolom'}
+            ]
+        });
+    });
+</script>
 
 
--- resources/views/master/objek/form.blade.php
+++ resources/views/master/objek/form.blade.php
@@ -21,3 +21,28 @@
     <div class="bungkus">
     </div>
 </div>
+<table id="table-list" class="table table-bordered table-consoned">
+    <thead>
+        <tr>
+            <th>Nama Table</th>
+            <th>Nama Kolom</th>
+        </tr>
+    </thead>
+    <tbody>
+
+    </tbody>
+</table>
+<script>
+    $(function(){
+        $('#table-list').DataTable({
+            processing: true,
+            searchDelay: 1000,
+            serverSide: true,
+            ajax: '<?= route('objek.get_data') ?>',
+            columns:[
+                {data: 'nama_table', name: 'nama_table'},
+                {data: 'nama_kolom', name: 'nama_kolom'}
+            ]
+        });
+    });
+</script>
